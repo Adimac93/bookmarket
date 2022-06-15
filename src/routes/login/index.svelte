b9e8d17b88b2c1c217795850c7eff19b3f547c1b
pkb-pmj <103094889+pkb-pmj@users.noreply.github.com>
2022-06-15 23:10:33 +0200

    routes: create basic login form page

---
+++ src/routes/login/index.svelte
@@ -0,0 +1,24 @@
+<form>
+	<h1>Zaloguj się</h1>
+	<label>Login<input type="text" required pattern="\w+" /></label>
+	<label>Hasło<input type="password" required minlength="8" /></label>
+	<button>Zaloguj się</button>
+</form>
+
+<style>
+	form {
+		margin: auto;
+		width: fit-content;
+		font-family: ui-rounded;
+	}
+	input {
+		display: block;
+	}
+	input[type="text"], input[type="password"] {
+		border: 1px solid #555;
+		border-radius: 2px;
+	}
+	input:invalid {
+		border-color: red;
+	}
+</style>
