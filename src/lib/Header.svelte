2e71d63165204f13c68f4ce29824e05c55c865a3
pkb-pmj <103094889+pkb-pmj@users.noreply.github.com>
2022-06-15 21:54:44 +0200

    lib: add `Header` component

---
+++ src/lib/Header.svelte
@@ -0,0 +1,32 @@
+<ul>
+	<li><a href="/">Strona główna</a></li>
+	<li><a href="/sklep">Sklep</a></li>
+	<li><a href="/o-nas">O nas</a></li>
+	<li><a href="/login">Zaloguj się</a></li>
+</ul>
+
+<style>
+	ul {
+		margin: 0;
+		padding: 0;
+		list-style: none;
+		display: flex;
+		flex-flow: row nowrap;
+		justify-content: flex-start;
+		background-color: white;
+	}
+	li:last-child {
+		margin-left: auto;
+	}
+	a {
+		display: block;
+		padding: 0.5em 1em;
+		transition: 0.2s;
+		text-decoration: none;
+		color: black;
+		font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+	}
+	a:hover {
+		color: #00D;
+	}
+</style>
