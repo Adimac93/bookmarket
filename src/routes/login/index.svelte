fcbe338d1c3ae1cc2ab69d0a86dad01eab76eac1
pkb-pmj <103094889+pkb-pmj@users.noreply.github.com>
2022-06-17 14:35:27 +0200

    routes: make FB auth work

--- src/routes/login/index.svelte
+++ src/routes/login/index.svelte
@@ -1,11 +1,35 @@
 <script lang="ts">
-	const onLoad = () =>
-	// @ts-expect-error
+	const onLoad = () => {
+		// @ts-expect-error
 		FB.init({
 			appId: '331682405650576',
 			xfbml: true,
 			version: 'v14.0'
 		});
+		// @ts-expect-error
+		FB.getLoginStatus(async function (response) {
+			if (response.status === 'connected') {
+				// The user is logged in and has authenticated your
+				// app, and response.authResponse supplies
+				// the user's ID, a valid access token, a signed
+				// request, and the time the access token
+				// and signed request each expire.
+				const uid = response.authResponse.userID;
+				const accessToken = response.authResponse.accessToken;
+				console.log(response)
+				const response_ = await fetch(`https://graph.facebook.com/v14.0/${uid}?metadata=1&access_token=${accessToken}`);
+				console.log(await response_.json())
+			} else if (response.status === 'not_authorized') {
+				// The user hasn't authorized your application.  They
+				// must click the Login button, or you must call FB.login
+				// in response to a user gesture, to launch a login dialog.
+			} else {
+				// The user isn't logged in to Facebook. You can launch a
+				// login dialog with a user gesture, but the user may have
+				// to log in to Facebook before authorizing your application.
+			}
+		});
+	};
 </script>
 
 <svelte:head>
@@ -23,7 +47,7 @@
 <div
 	id="g_id_onload"
 	data-client_id="266186020689-9dt4vgv7nasollcmg96mp66idnes48is.apps.googleusercontent.com"
-	data-login_uri="http://localhost:3000/login?provider=google"
+	data-login_uri="http://localhost:3000/login/google"
 	data-auto_prompt="false"
 />
 <div
@@ -44,4 +68,3 @@
 	data-auto-logout-link="false"
 	data-use-continue-as="false"
 />
-<a href="https://www.facebook.com/v14.0/dialog/oauth?client_id=331682405650576&redirect_uri=https://localhost:3000/login?provider=facebook&state=test">Kontynuuj przy użyciu Facebooka</a>
