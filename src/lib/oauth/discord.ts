4b887d7c86b00185c45b04daec54f2d002be4103
Adimac93 <Adimac93@gmail.com>
2022-06-18 12:52:27 +0200

    fix: fetch discord oauth

--- src/lib/oauth/discord.ts
+++ src/lib/oauth/discord.ts
@@ -1,33 +1,21 @@
-import { db } from '$lib/database';
 export async function auth(code: string) {
 	console.log('Discord auth initialised');
 	let tokenReq = await getToken(code);
-	console.log(tokenReq);
 	if (tokenReq.ok) {
-		let userReq = await getUser(tokenReq.data.access_token);
-		console.log(userReq);
+		const tokenData = await tokenReq.json();
+		console.log(tokenData);
+
+		let userReq = await getUser(tokenData.access_token);
 		if (userReq.ok) {
-			const userData = userReq.data;
-			console.log(userData.id, userData.email, userData.username);
-
-			let user = await db.user.findFirst({ where: { discord_id: userData.id } });
-			if (user) {
-				console.log('Account already created');
-			} else {
-				console.log('Creating account');
-				await db.user.create({
-					data: { name: userData.username, discord_id: userData.id }
-				});
-			}
-		} else {
-			console.log('Bad request');
+			return (await userReq.json()).id;
 		}
-	} else {
-		console.log('Bad token');
+		console.log('Bad request');
+		return;
 	}
+	console.log('Bad token');
+	return;
 }
-
-async function getToken(code: string): Promise<fetchResult> {
+async function getToken(code: string) {
 	var body = new URLSearchParams();
 	body.append('client_id', '987076912247627826');
 	body.append('client_secret', 'wi15ZeH5tnQSRvK8HZSvmpKCvRsvld5s');
@@ -35,39 +23,16 @@ async function getToken(code: string): Promise<fetchResult> {
 	body.append('code', code);
 	body.append('redirect_uri', 'https://localhost:3000/login?provider=discord');
 
-	return await $fetch('https://discord.com/api/oauth2/token', {
+	return await fetch('https://discord.com/api/oauth2/token', {
 		method: 'POST',
 		headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
 		body
 	});
 }
 
-async function getUser(access_token: string): Promise<fetchResult> {
-	return await $fetch('https://discordapp.com/api/users/@me', {
+async function getUser(access_token: string) {
+	return await fetch('https://discordapp.com/api/users/@me', {
 		method: 'GET',
 		headers: { Authorization: `Bearer ${access_token}` }
 	});
-}
-
-async function $fetch(url: string, payload: RequestInit): Promise<fetchResult> {
-	return await fetch(url, payload)
-		.then(async function (response) {
-			return {
-				data: await response.json(),
-				ok: response.ok,
-				status: response.statusText
-			};
-		})
-		.catch((e) => {
-			return {
-				data: e,
-				ok: false,
-				status: 'Fetch failed'
-			};
-		});
-}
-interface fetchResult {
-	data: any;
-	ok: boolean;
-	status: string;
 }
