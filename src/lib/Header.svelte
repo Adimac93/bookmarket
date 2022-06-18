a1df373b99b34684f4224c5b3ae1bd64412e638a
Adimac93 <Adimac93@gmail.com>
2022-06-18 23:43:34 +0200

    header: use `session.isLoggedIn`

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,9 +1,17 @@
+<script lang="ts">
+	import { session } from "$app/stores";
+</script>
 <ul>
 	<li><a href="/">Strona główna</a></li>
 	<li><a href="/sklep">Sklep</a></li>
 	<li><a href="/o-nas">O nas</a></li>
-	<li><a href="/login">Zaloguj się</a></li>
-	<li><a href="/signup">Załóż konto</a></li>
+	{#if !$session.isLoggedIn}
+		<li><a href="/login">Zaloguj się</a></li>
+		<li><a href="/signup">Załóż konto</a></li>
+	{:else}
+		<li></li>
+		<li><a href="/logout" >Wyloguj się</a></li>
+	{/if}
 </ul>
 
 <style>
