d3969d5c4c23ae7c77edbe7880fa00f465ca3ccf
Adimac93 <Adimac93@gmail.com>
2022-06-18 23:40:48 +0200

    hooks: add `getSession` hook and `Session` interface

---
+++ src/hooks.ts
@@ -0,0 +1,8 @@
+import type { GetSession, Handle } from '@sveltejs/kit';
+import * as cookie from 'cookie';
+
+export const getSession: GetSession = async ({ request }) => {
+	const sessionId = cookie.parse(request.headers.get('Cookie') || '')['session_id'] || '';
+	console.log(sessionId);
+	return { isLoggedIn: !!sessionId };
+};
