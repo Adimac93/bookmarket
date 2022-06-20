35299b8f6854d58722c79f02f9db3813f5ee8336
pkb-pmj <103094889+pkb-pmj@users.noreply.github.com>
2022-06-20 23:25:57 +0200

    db: create `sessions` class to be improved later

--- src/lib/database.ts
+++ src/lib/database.ts
@@ -8,3 +8,22 @@ export interface Signup {
 	id: string;
 	provider: Provider;
 }
+
+class Sessions {
+	private sessions: Record<string, string> = {};
+	get(cookie: string): string | undefined {
+		return this.sessions[cookie];
+	}
+	create(id: string) {
+		const cookie = crypto.randomUUID();
+		this.sessions[cookie] = id;
+		return cookie;
+	}
+	delete(cookie: string) {
+		const id = this.sessions[cookie];
+		delete this.sessions[cookie];
+		return id;
+	}
+}
+
+export const sessions_class = new Sessions();
