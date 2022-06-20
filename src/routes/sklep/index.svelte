9097e58edc65e14f4b4657c4f554d233b8feb422
Adimac93 <Adimac93@gmail.com>
2022-06-20 20:42:22 +0200

    routes: replace demo books display gallery

--- src/routes/sklep/index.svelte
+++ src/routes/sklep/index.svelte
@@ -6,33 +6,58 @@
 
 <ul>
 	{#each books as book}
-		<div>
-			<h1>{book.title}</h1>
-			<h2>{book.author}</h2>
-			<p>{book.description}</p>
-			<img src={book.image} alt="Zdjęcie" />
-		</div>
+	<div class="book">
+        <div class="cover">
+          <img src="{book.image}" alt="Okładka książki">
+        </div>
+        <div class="description">
+          <p class="title">{book.title}<br>
+            <span class="author">{book.author}</span>
+		</p>
+        </div>
+	</div>
 	{:else}
 		<h1>No books for ya</h1>
 	{/each}
 </ul>
 
 <style>
-	ul {
-		list-style: none;
-		display: flex;
-		flex-flow: row wrap;
-		font-size: 0.8em;
-	}
-	div {
-		width: 20em;
-		height: 20em;
-		background-color: #fff;
-		border-radius: 0.5em;
-		padding: 0.5em;
-	}
-	img {
-		width: auto;
-		height: auto;
-	}
+
+.book {
+  display: inline-block;
+  width: 230px;
+  height: auto;
+  box-shadow: 0 0 20px #aaa;
+  margin: 25px;
+  padding: 10px 10px 0 10px;
+  vertical-align: top;
+  transition: height 1s;
+}
+
+.cover {
+  border: 2px solid gray;
+  height: 80%;
+  overflow: hidden;
+}
+
+.cover img {
+  width: 100%;
+}
+
+.book p {
+  margin-top: 12px;
+  font-size: 20px;
+}
+
+.book .author {
+  font-size: 15px;
+}
+
+h1 {
+  color: gray;
+  text-align: center;
+  font-size: 50px;
+  margin-bottom: -3px;
+}
+
 </style>
