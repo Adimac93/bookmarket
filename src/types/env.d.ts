abd3e02447ff3f6afdde454a02b2bade63ff27a9
Adimac93 <Adimac93@gmail.com>
2022-06-29 23:49:41 +0200

    env: add GitHub app secrets

--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -14,6 +14,12 @@ declare global {
 			FACEBOOK_CLIENT_SECRET: string;
 
 			BOOKS_API_KEY: string;
+
+			GITHUB_APP_ID: string;
+			GITHUB_CLIENT_ID: string;
+			GITHUB_CLIENT_SECRET: string;
+			GITHUB_APP_PRIVATE_KEY: string;
+			GITHUB_INSTALLATION_ID: string;
 		}
 	}
 }
