d5109eb512c6bc3d7509b3de01d3b52ac9613105
pkb-pmj <103094889+pkb-pmj@users.noreply.github.com>
2022-07-01 20:08:24 +0200

    api: add `/book/filter` endpoint

---
+++ src/routes/api/book/filter.ts
@@ -0,0 +1,28 @@
+import type { RequestHandler } from '@sveltejs/kit';
+import { Grade, Subject } from '@prisma/client';
+import { db } from '$lib/database';
+
+export const get: RequestHandler = async ({ url }) => {
+	const isbn = url.searchParams.get('isbn');
+	const title = url.searchParams.get('title');
+	const authors = url.searchParams.get('authors');
+	const grade = url.searchParams.get('grade')?.toUpperCase();
+	const subject = url.searchParams.get('subject')?.toUpperCase();
+
+	const books = await db.book.findMany({
+		where: {
+			AND: [
+				{ id: isbn ?? undefined },
+				{ title: title ? { contains: title, mode: 'insensitive' } : undefined },
+				{ author: authors ? { contains: authors, mode: 'insensitive' } : undefined },
+				{ grade: (grade ?? '') in Grade ? (grade as Grade) : undefined },
+				{ subject: (subject ?? '') in Subject ? (subject as Subject) : undefined },
+			],
+		},
+	});
+
+	return {
+		status: 200,
+		body: { books },
+	};
+};
