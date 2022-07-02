1ff3a289afb4ce97644cc4bdfdbf7b8038c4a464
Adimac93 <Adimac93@gmail.com>
2022-07-02 18:04:57 +0200

    extract bookloader to `bookloader-cli` repo
    ->https://github.com/Adimac93/bookmarket-cli

--- src/routes/bookloader/index.ts
+++
@@ -1,7 +0,0 @@
-import type { RequestHandler } from '@sveltejs/kit';
-import { generateSet } from '$lib/store/bookLoader';
-
-export const get: RequestHandler = async ({ url }) => {
-	await generateSet();
-	return { status: 200 };
-};
