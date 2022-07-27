f3675dd82c19987c886fe8cd4d0f01ddc72d161e
pkb-pmj <103094889+pkb-pmj@users.noreply.github.com>
2022-07-27 22:15:43 +0200

    UI:lib&static: create floating button

---
+++ src/lib/floating/FloatingButton.svelte
@@ -0,0 +1,21 @@
+<button on:click>
+    <slot />
+</button>
+
+<style>
+    button {
+        appearance: none;
+        border: none;
+        border-radius: 50%;
+        height: 3.2rem;
+        width: 3.2rem;
+        margin: 0;
+		padding: 0.5rem;
+		display: flex;
+		place-content: center;
+        position: fixed;
+        bottom: 3.6rem;
+        right: 0.6rem;
+        background-color: #f50;
+    }
+</style>
