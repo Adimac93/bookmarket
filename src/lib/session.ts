98e437d0a1e841f8c59e1a98c14c97d487f500df
pkb-pmj <103094889+pkb-pmj@users.noreply.github.com>
2022-07-30 20:56:49 +0200

    session(lib): create session manager

---
+++ src/lib/session.ts
@@ -0,0 +1,81 @@
+interface SessionManager {
+	/** @returns sessionID */
+	logIn(userID: string, expires: Date): string;
+
+	getUserID(sessionID: string): string | undefined;
+
+	getAllDevices(userID: string): string[] | undefined;
+
+	logOut(sessionID: string): void;
+
+	logOutAllDevices(userID: string): void;
+}
+
+interface Session {
+	userID: string;
+	expires: number;
+}
+
+const hashMap: Record<string, Session> = Object.create(null);
+
+export const session: SessionManager = {
+	logIn(userID, expires) {
+		const sessionID = crypto.randomUUID();
+
+		hashMap[sessionID] = {
+			userID,
+			expires: expires.valueOf(),
+		};
+		return sessionID;
+	},
+	getUserID(sessionID) {
+		const user = hashMap[sessionID];
+		if (!user) {
+			return;
+		}
+		if (user.expires > Date.now()) {
+			delete hashMap[sessionID];
+			return;
+		}
+		return user.userID;
+	},
+	getAllDevices(userID) {
+		const now = Date.now();
+		let result: string[] = [];
+
+		for (const sessionID in hashMap) {
+			if (hashMap[sessionID].expires > now) {
+				delete hashMap[sessionID];
+			} else if (hashMap[sessionID].userID === userID) {
+				result.push(sessionID);
+			}
+		}
+		return result;
+	},
+	logOut(sessionID) {
+		const userID = this.getUserID(sessionID);
+		if (!userID) {
+			return;
+		}
+		delete hashMap[sessionID];
+	},
+	logOutAllDevices(userID) {
+		const devices = this.getAllDevices(userID);
+		if (!devices) {
+			return;
+		}
+		for (const sessionID of devices) {
+			delete hashMap[sessionID];
+		}
+	},
+};
+
+export const sweep = () => {
+	const now = Date.now();
+
+	for (const sessionID in hashMap) {
+		if (hashMap[sessionID].expires > now) {
+			delete hashMap[sessionID];
+		}
+	}
+};
