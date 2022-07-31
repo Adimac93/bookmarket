4f12296e12a9c3213146ea763efefd34e7ab1237
Adimac93 <Adimac93@gmail.com>
2022-07-31 20:24:10 +0200

    db: import subjects assets

--- src/lib/events.ts
+++ src/lib/events.ts
@@ -1,5 +1,16 @@
 import dotenv from 'dotenv';
+import { db } from './database';
+import subjects from '$lib/assets/subjects.json';
 export async function beforeStart() {
 	let start = Date.now();
 	dotenv.config();
+	await loadSubjects();
+	console.log(`Setup finished in ${(Date.now() - start) / 1000}s`);
+}
+
+async function loadSubjects() {
+	const data = Object.entries(subjects).map(([key, value]) => {
+		return { name: key, alias: value[0] };
+	});
+	await db.subject.createMany({ data, skipDuplicates: true });
 }
