1be3df9de273d159e75592ad8e5a231334dea412
pkb-pmj <103094889+pkb-pmj@users.noreply.github.com>
2022-08-04 19:38:15 +0200

    UI(`/zapisane`): add submit button

--- src/routes/zapisane/index.svelte
+++ src/routes/zapisane/index.svelte
@@ -9,11 +9,16 @@
 
 	let profil: number;
 	let język: number;
+	$: active = profil !== -1 && język !== -1;
 </script>
 
 <div>
 	<Select placeholder="Profil" bind:value={profil} choices={profileMap} />
 	<SelectLang placeholder="Język obcy" bind:value={język} choices={językiMap} />
+	<button class:active>Stwórz zamówienie</button>
+	{#if profil !== -1 && język !== -1}
+		<!-- {#await fetch('/').then((r) => r.json())}{/await} -->
+	{/if}
 </div>
 
 <style>
@@ -22,5 +27,21 @@
 		flex-flow: column nowrap;
 		padding: 0.5rem;
 		gap: 0.5rem;
+	}
+	button {
+		appearance: none;
+		border: none;
+		margin: 0;
+		padding: 0.8rem;
+		font-size: 1rem;
+		border-radius: 0.5rem;
+		background-color: #aaa;
+		color: #000;
+		transition: 0.4s;
+		cursor: pointer;
+	}
+	button.active {
+		background-color: #f50;
+		color: #fff;
 	}
 </style>
