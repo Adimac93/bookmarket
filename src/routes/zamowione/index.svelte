8c10b48de3c54e2445196b2c9e86a408bb294ea6
pkb-pmj <103094889+pkb-pmj@users.noreply.github.com>
2022-08-07 16:28:50 +0200

    feat(`/zamowione`): display orders (unstyled)

---
+++ src/routes/zamowione/index.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+	import BookComponent from '$lib/m/BookComponent.svelte';
+	import type { Book, Order, OrderBook } from '@prisma/client';
+
+	export let orders: (Order & {
+		books: (OrderBook & {
+			book: Book;
+		})[];
+	})[];
+</script>
+
+<ol>
+	{#each orders as order}
+		<li>
+			<details>
+				<summary>{order.created.toLocaleDateString('pl')}</summary>
+				<ul>
+					{#each order.books as { book }}
+						<BookComponent {book} />
+					{/each}
+				</ul>
+			</details>
+		</li>
+	{/each}
+</ol>
+
+<style>
+	ol {
+		display: block;
+		margin: 0;
+		padding: 0;
+		gap: 0.2rem;
+		list-style: none;
+	}
+	ul {
+		display: grid;
+		grid-template-columns: repeat(auto-fit, minmax(10rem, 1fr));
+		margin: 0;
+		padding: 0;
+		gap: 0.2rem;
+		list-style: none;
+	}
+</style>
---
+++ src/routes/zamowione/index.ts
@@ -0,0 +1,34 @@
+import { db } from '$lib/database';
+import type { Book, Order, OrderBook } from '@prisma/client';
+import type { RequestHandler } from './__types';
+
+export const get: RequestHandler<{
+	orders: (Order & {
+		books: (OrderBook & {
+			book: Book;
+		})[];
+	})[];
+}> = async ({ locals }) => {
+	if (!locals.user) return { status: 401 };
+
+	const orders = await db.order.findMany({
+		where: {
+			buyerId: locals.user.id,
+		},
+		include: {
+			books: {
+				include: {
+					book: true,
+				},
+			},
+		},
+		orderBy: {
+			created: 'desc',
+		},
+	});
+
+	return {
+		status: 200,
+		body: { orders },
+	};
+};
