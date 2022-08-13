2af9c393f27e18ab5f21add7d65855a572b92bbd
pkb-pmj <103094889+pkb-pmj@users.noreply.github.com>
2022-08-13 18:28:15 +0200

    `/sprzedane`: add `POST` handler to create order

--- src/routes/sprzedane/index.ts
+++ src/routes/sprzedane/index.ts
@@ -1,5 +1,14 @@
 import { db } from '$lib/database';
-import type { Base, Book, Order, OrderBook, School, User } from '@prisma/client';
+import type {
+	Base,
+	Book,
+	BookForSale,
+	Order,
+	OrderBook,
+	Prisma,
+	School,
+	User,
+} from '@prisma/client';
 import type { RequestHandler } from './__types';
 
 export const get: RequestHandler<{
@@ -42,3 +51,61 @@ export const get: RequestHandler<{
 		body: { orders },
 	};
 };
+
+export const post: RequestHandler = async ({ locals, request }) => {
+	if (!locals.user) return { status: 401 };
+
+	const data: ShadowPostData = await request.json();
+
+	const mapped: Prisma.OrderBookCreateWithoutOrderInput[] = data.books.map(
+		({ condition, count, isbn }) => ({
+			book: {
+				connect: {
+					isbn,
+				},
+			},
+			condition,
+			count,
+		}),
+	);
+
+	await db.$transaction([
+		db.order.create({
+			data: {
+				status: 'PENDING',
+				buyer: {
+					connect: {
+						id: data.buyerId,
+					},
+				},
+				seller: {
+					connect: {
+						id: locals.user.id,
+					},
+				},
+				books: {
+					create: mapped,
+				},
+			},
+		}),
+		// TODO: update count instead of deleting
+		db.bookForSale.deleteMany({
+			where: {
+				sellerId: locals.user.id,
+				OR: data.books.map(({ condition, isbn }) => ({
+					condition,
+					isbn,
+				})),
+			},
+		}),
+	]);
+
+	return {
+		status: 201,
+	};
+};
+
+export interface ShadowPostData {
+	buyerId: string;
+	books: Omit<BookForSale, 'sellerId'>[];
+}
